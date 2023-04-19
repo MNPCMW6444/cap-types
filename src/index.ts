63ffa60bfd5988b7e2614f84ce9bef68f877eaed
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-19 16:02:31 +0300

    ok

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export interface FormData {
+  annualRevenue: string;
+  currency: string;
+  annualGrowthRate: string;
+  currentRunway: string;
+  termLength: number;
+  gracePeriod: string;
+  email: string;
+}
