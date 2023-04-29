3c3d97eb1e06a8e857c19057d47262c07ec714ce
YoadNatan <yoad12121223@gmail.com>
2023-04-29 19:52:59 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 import { Document } from "mongoose";
-
 export interface CapHubUser extends Document {
   email: string;
   name: string;
   passwordHash: string;
 }
+enum Currency {
+  EUR = "EUR",
+  GBP = "GBP",
+  CHF = "CHF",
+  RUB = "RUB",
+  TRY = "TRY",
+  ZAR = "ZAR",
+  ILS = "ILS",
+  AED = "AED",
+}
 
 export interface WebsiteFormData {
   annualRevenue: number;
-  currency: "EUR" | "GBP" | "CHF" | "RUB" | "TRY" | "ZAR" | "ILS" | "AED";
+  currency: Currency;
   annualGrowthRate: number;
   currentRunway: number;
   termLength: number;
   gracePeriod: number;
   email: string;
 }
-
 export interface SavedWebsiteFormData extends Document {
   stringifiedFormData: string;
 }
