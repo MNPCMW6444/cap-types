96b49baf3b4f4d7ae38aa691a5c42259f405612c
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-05-01 18:27:44 +0300

    Revert "awd"
    This reverts commit 9d64877c6d869fef968847b401cfcea4eac0451e.

--- src/positiveNumber.ts
+++
@@ -1,3 +0,0 @@
-export type PositiveNumber = number & { readonly __brand: unique symbol };
-export const toPositiveNumber = (value: number): PositiveNumber | null =>
-  value > 0 ? (value as PositiveNumber) : null;
