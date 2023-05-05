e847d768b685d041b6644ebe7aa26895103a1111
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-05-05 16:36:31 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Document } from "mongoose";
 
-export interface CapHubUser extends Document {
+export interface CaphubUser extends Document {
   email: string;
   name: string;
   passwordHash: string;
